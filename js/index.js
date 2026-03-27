b1656e18af9bfef5feca0e6995dd7aa278e65778
Cauã Carlos <carlos.caua@escolar.ifrn.edu.br>
2026-03-27 17:41:44 -0300

    Adição de botão de reset, limite de número, mensagem de limite e melhora na estrutura do código

--- js/index.js
+++ js/index.js
@@ -1,26 +1,68 @@
 let numero  = 0
+
+let nMax = 10
+
+let nMin = -10
+
 const elemento =document.getElementById("valor") 
-function attTela(){
-    document.getElementById("valor").innerHTML = numero;
-    if (numero>0){
-        elemento.style.color = "blue"
-    } else if  ( numero === 0 ) {
-        elemento.style.color = "gold"
-    } else {
-        elemento.style.color = "red"
+
+const tipoNumero = document.getElementById("tipo")
+
+
+
+
+function alterarNumero(valor){
+    if (numero >= nMax && valor > 0){
+        add.disabled = true
+        return
     }
+    if (numero <= nMin && valor < 0){
+        alert("Limite atingido")
+        return
+    }
+    numero += valor 
+    attTela()
+}
+
+const add = document.getElementById("somar")
+
+add.addEventListener("click", () => alterarNumero(1))
+
+
+const menos = document.getElementById("subtrair")
+
+menos.addEventListener("click", () => alterarNumero(-1))
+
+function resetar(){
+    numero = 0
+    attTela()
+}
+
+const resetBtn = document.getElementById("reset")
+
+resetBtn.addEventListener("click", resetar)
+
+
+function attValor(){
+    elemento.textContent = numero
+}
+
+function attTipo(){
+    if (numero>0){
+        elemento.style.color = "green"
+        tipoNumero.textContent = "Positivo"
+    } else if (numero === 0){
+        elemento.style.color = "gold"
+        tipoNumero.textContent = "Neutro"
+    } else{
+        elemento.style.color = "red"
+        tipoNumero.textContent = "Negativo"
+    }
+}
+
+function attTela(){
+    attValor()
+    attTipo()
 } 
 
-function adicionar(){
-    numero += 1
-}
-const add = document.getElementById("somar")
-add.addEventListener("click", adicionar)
 
-function retirar(){
-    numero -=1
-}
-const menos = document.getElementById("subtrair")
-menos.addEventListener("click", retirar)
-
-attTela()
