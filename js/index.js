1ae6eb35ecb265cd5fafe11ef65f85c9c3f828c3
Cauã Carlos <carlos.caua@escolar.ifrn.edu.br>
2026-03-31 17:38:13 -0300

    Base bem construída. Falta ajeitar o layout e terminar as função da to do list

--- js/index.js
+++ js/index.js
@@ -1,54 +1,18 @@
-let contador = 0
-let input = document.getElementById("addTarefa")
+inputTarefa = document.getElementById("tarefa")
 
-let btnAdd = document.getElementById("add")
 
-let main = document.getElementById("lista")
+btnAdd = document.getElementById("adicionar")
 
 function addTarefa(){
-    let valorInput = input.value; //Pegar o valor dado no input
-
-    if (valorInput !=="" && valorInput !== null && valorInput !== undefined){
-        ++contador //Cada vez que um item é criado, ele precisa de um indice. Cada vez q a função for chamada, o contador aumenta.
-        let novoItem = `<div class="item" id="${contador}">
-            <div class="item-icone" onclick="marcarTarefa(${contador})">
-                <span class="material-symbols-outlined">circle</span>
-            </div>
-
-            <div class="item-nome" >
-                ${valorInput} <!-- pegar a variavel-->
-            </div>
-
-            <div class="item-botao">
-                
-                <button class="delete" onclick="deletar(${contador})"><span class="material-symbols-outlined">delete</span>Deletar</button>
-            </div>
+    valorInput = inputTarefa.value
+    lista = document.getElementById("conteudo")
+    lista.innerHTML += `<div class="flex justify-center items-center ">
+    <input type="checkbox">
+        <p class="w-2xl">${valorInput}</p>
+        <button class="bg-red-700 rounded-3xl px-10 py-3 shadow-black shadow-xl transition transition-transform duration-300 hover:scale-120 ">Deletar</button>
         </div>`
 
-        main.innerHTML += novoItem // Adiciona tudo ao main
-
-        input.value = "" //Limpa o input após o click
-
-        input.focus() // Faz com que após clicar pra adicionar algo no input, o foco volte ao input automaticamente. Unido ao input.value = "", ele volta e o campo já está limpo
-    }
-        
+    
 }
 
 btnAdd.addEventListener("click", addTarefa)
-
-input.addEventListener("keyup", function(event){ // Função pra conseguir utilizar entender ao invés de clicar no botão
-    if (event.key === "Enter") { 
-        addTarefa() //Se o usuário clicar em enter, vai disparar a função addTarefa()
-    }
-})
-
-function deletar(id){
-    var tarefa = document.getElementById(id)
-    tarefa.remove()
-}
-
-function marcarTarefa(id){
-    let item = document.getElementById(id)
-
-    item.classList.toggle("done")
-}
