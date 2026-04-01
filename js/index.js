ad8bee680d7c220998a3bdb86978ee6e99c8759b
Cauã Carlos <carlos.caua@escolar.ifrn.edu.br>
2026-04-01 16:24:38 -0300

    Melhora de concluir,salvar os itens e leve mudança na interface

--- js/index.js
+++ js/index.js
@@ -1,26 +1,28 @@
 let inputTarefa = document.getElementById("tarefa")
 let cont = 0
 let btnAdd = document.getElementById("adicionar")
-
+let lista = document.getElementById("conteudo")
 
 function addTarefa(){
     cont++ // incrementa valor de cont
-    valorInput = inputTarefa.value // Pega valor do input
-    let lista = document.getElementById("conteudo") // 
+    let valorInput = inputTarefa.value // Pega valor do input
+     // 
     if (valorInput !== null && valorInput !== "" && valorInput !== undefined){
     lista.innerHTML += `<div class="flex justify-between w-7xl items-center shadow-black shadow-md rounded-3xl py-7 px-8 font-bold m-5" id = "${cont}">
-    <input type="checkbox" onchange="concluir(${cont})">
-        <p class="w-2xl" id="texto-${cont}">${valorInput}</p>
-        <button class="bg-red-700 rounded-3xl px-10 py-3 shadow-black shadow-md transition transition-transform duration-300 hover:scale-120" id="deletar" onclick="deletar(${cont})">Deletar</button>
+    <input type="checkbox" onchange="concluir(${cont})" class="cursor-pointer transition transition-transform duration-300 hover:scale-120" >
+        <p class="font-bold" id="texto-${cont}">${valorInput}</p>
+        <button class="bg-red-600 rounded-3xl px-7 py-3 shadow-black shadow-md transition transition-transform duration-300 hover:scale-120 cursor-pointer" id="deletar" onclick="deletar(${cont})">X</button>
         </div>`
     }
     inputTarefa.value = ""
-    inputTarefa.focus()
+    inputTarefa.focus();
+    salvar()
 }
 
 function deletar(id){
     var tarefa = document.getElementById(id)
-    tarefa.remove()
+    tarefa.remove();
+    salvar()
 }
 
 inputTarefa.addEventListener("keyup", function(event){
@@ -32,9 +34,25 @@ inputTarefa.addEventListener("keyup", function(event){
 function concluir(id){
     let texto = document.getElementById(`texto-${id}`)
     texto.classList.toggle("line-through")
-    texto.classList.toggle("opcaity-50")
+    texto.classList.toggle("text-gray-500")
+    texto.classList.toggle("normal")
     let elemento = document.getElementById(id)
+    let lista_tarefas = document.getElementById("conteudo")
     elemento.classList.toggle("bg-green-200")
+    lista_tarefas.appendChild(elemento);
+    salvar()
 }
 
 btnAdd.addEventListener("click", addTarefa)
+
+function salvar(){
+    localStorage.setItem("conteudo_lista", lista.innerHTML)
+}
+
+function carregar(){
+    lista.innerHTML = localStorage.getItem("conteudo_lista") || ""
+}
+
+window.onload = function (){
+    carregar();
+}
