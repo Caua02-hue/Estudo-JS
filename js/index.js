f4618f6140a8c3b02707920c1aa3e0ab07828c4b
Cauã Carlos <carlos.caua@escolar.ifrn.edu.br>
2026-04-07 14:44:17 -0300

    Adição de filtros

--- js/index.js
+++ js/index.js
@@ -8,8 +8,6 @@ let btnAddLista = document.getElementById("addLista")
 
 let filtrarTodos = document.getElementById("filtrarTodos")
 
-let filtrarPendente = document.getElementById("filtrarPendente")
-
 let filtrarAssistidos = document.getElementById("filtrarAssistidos")
 
 let filtficcao = document.getElementById("filtficcao")
@@ -32,16 +30,36 @@ let filmes = []
 
 
 function render(){
-    listaFilmes.innerHTML = filmes.map(filme =>{
+    let lista = filmes 
+    if (filtro === "assistidos"){
+        lista = filmes.filter(f => f.assistido)
+    }
+    if (filtro === "Ficção"){
+        lista = filmes.filter(f => f.genero === "Ficção")
+    }
+    if (filtro === "Ação"){
+        lista = filmes.filter(f => f.genero === "Ação")
+    }
+    if (filtro === "Terror"){
+        lista = filmes.filter(f => f.genero === "Terror")
+    }
+    if (filtro === "Romance"){
+        lista = filmes.filter(f => f.genero === "Romance")
+    }
+
+    listaFilmes.innerHTML = lista.map(filme =>{
         return `<div class="flex border justify-around px-20 py-7 font-bold w-full  bg-[#FFFFFF] rounded-xl">
-  <div class="flex gap-50 text-4xl">
+  <div class="flex text-4xl gap-20">
     <span>${filme.titulo}</span>
     <span>${filme.genero}</span>
   </div>
 
-  <div class="flex gap-10">
+  <div class="flex gap-25">
     <p class="text-3xl">${filme.nota}</p>
-    <button class="border p-2 rounded-lg transform transition duration-300 hover:scale-115 cursor-pointer px-10 hover:bg-[#10B981] hover:text-white" onclick="concluir(${filme.id})">Marcar como assistido</button>
+    ${ filme.assistido?
+        `<button class= "border p-2 rounded-lg transform transition duration-300 hover:scale-115 cursor-pointer px-10 hover:bg-[#EF4444] hover:text-white" onclick="concluir(${filme.id})">Assistido ✅</button>` :
+    `<button class="border p-2 rounded-lg transform transition duration-300 hover:scale-115 cursor-pointer px-10 hover:bg-[#10B981] hover:text-white" onclick="concluir(${filme.id})">Marcar como assistido</button>`
+}   
     <button class="border p-2 rounded-lg transform transition duration-300 hover:scale-115 cursor-pointer px-10 hover:bg-[#EF4444] hover:text-white" onclick="deletar(${filme.id})">X</button>
   </div>
   </div>`
@@ -64,9 +82,57 @@ function add(){
 
 btnAddLista.addEventListener("click", add)
 
-function deletar(a){
-    filmes = filmes.filter(filme => filme.id !== a)
+function deletar(id){
+    filmes = filmes.filter(filme => filme.id !== id)
     render()
 }
 
+function concluir(id){
+    filmes = filmes.map(filme =>{
+        if (filme.id === id){
+            return {...filme, assistido : !filme.assistido}
+        } else{
+           return filme
+        }
+    })
+    render()
+}
 
+let filtro = "todos"
+
+function mostrarTodos(){
+    filtro= "todos"
+    render()
+}
+
+function mostrarAssistidos(){
+    filtro = "assistidos"
+    render()
+}
+
+function mostrarFiccao(){
+    filtro = "Ficção"
+    render()
+}
+
+function mostrarAcao(){
+    filtro = "Ação"
+    render()
+}
+
+function mostrarTerror(){
+    filtro = "Terror"
+    render()
+}
+
+function MostrarRomance(){
+    filtro = "Romance"
+    render()
+}
+
+filtrarTodos.addEventListener("click", mostrarTodos)
+filtrarAssistidos.addEventListener("click", mostrarAssistidos)
+filtficcao.addEventListener("click", mostrarFiccao)
+filtAcao.addEventListener("click", mostrarAcao)
+filtTerror.addEventListener("click", mostrarTerror)
+filtRomance.addEventListener("click", MostrarRomance)
